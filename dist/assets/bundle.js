f19f375aeb06aa68b94e3ddbc58a0a326569996d
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-05-11 14:12:05 -0700

    Create initial config files

---
+++ dist/assets/bundle.js
@@ -0,0 +1,1 @@
+(()=>{const l=l=>{console.log(`Hello ${l}`)};l("mario"),l("luigi"),l("link")})();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const greet = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+greet("mario");
+greet("luigi");
+greet("link");
